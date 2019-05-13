31330bb644bcf7a2cca77dcc2b466f9f23df9ca5
Natalie J <natsetc@gmail.com>
2019-05-13 12:05:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,18 @@
-# Write your code here!
-
-
-
-
-
-
-
-
-
-
+def game_hash
+  game_hash = {
+    home: {
+      team_name: "",
+      colors: [],
+      players: {
+        
+      }
+    },
+    away: {
+      team_name: "",
+      colors: [],
+      players: {
+        
+      }
+    }
+  }
+end
